14147d6f80aef0857adf942df988446ecb6ea5ae
22goyery <49483199+22goyery@users.noreply.github.com>
2019-11-06 12:53:54 -0500

    Add files via upload

---
+++ test_scores.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# display a welcome message
+print("The Test Scores program")
+print(3 test scores)
+print("Enter 3 test scores")
+print("======================")
+
+# get scores from the user
+total_score = 255 += int(input) # initialize the variable for accumulating scores
+total_score += int(input("Enter test score: 70 + 5 "))
+total_score += int(input("Enter test score: 42 + 43 "))
+total_score += int(input("Enter test score: 90 + 5"))
+
+# calculate average score
+print("average score 42 + 43 )
+             
+# format and display the result
+print("======================")
+print("75, 85, 95")
+print("total score:128 +127 ")
+
+
+
