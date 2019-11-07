9a1ea23ec39aa2a902a2de552d7fae1caaca777c
22goyery <49483199+22goyery@users.noreply.github.com>
2019-11-07 12:34:56 -0500

    Add files via upload

---
+++ mpg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# display a welcome message
+print("The Miles Per Gallon program")
+print()
+
+# get input from the user
+miles_d= float(input("Enter miles driven:\t\t"))
+gallons_used = float(input("Enter gallons of gas used:\t\t"))
+cost_per_gallon = float(input("Enter cost per gallon:\t\t"))
+# calculate miles per gallon
+mpg = miles_driven / gallons_used
+mpg = round(mpg, 2)
+cpm = round(tcp / miles_drivers, 1)         
+# format and display the result
+print()
+print("Miles Per Gallon:\t\t" + str(mpg))
+print("total gas cost:\t\t\t" + str(tgc))
+print("cost per mile:\t\t\t" + str(cpm))
+ print()
+choice = input("Get entries for another trip?" (y/n): ")
+print() 
+print("bye")
+
