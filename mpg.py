39220c6d74506c5ac4d6023b7abee47457c4946a
22goyery <49483199+22goyery@users.noreply.github.com>
2019-11-08 08:54:19 -0500

    Add files via upload

--- mpg.py
+++ mpg.py
@@ -3,22 +3,21 @@
 # display a welcome message
 print("The Miles Per Gallon program")
 print()
-
-# get input from the user
-miles_d= float(input("Enter miles driven:\t\t"))
-gallons_used = float(input("Enter gallons of gas used:\t\t"))
-cost_per_gallon = float(input("Enter cost per gallon:\t\t"))
-# calculate miles per gallon
-mpg = miles_driven / gallons_used
-mpg = round(mpg, 2)
-cpm = round(tcp / miles_drivers, 1)         
-# format and display the result
-print()
-print("Miles Per Gallon:\t\t" + str(mpg))
-print("total gas cost:\t\t\t" + str(tgc))
-print("cost per mile:\t\t\t" + str(cpm))
- print()
-choice = input("Get entries for another trip?" (y/n): ")
+choice = "y"
+while choice.lower() == "y":
+    print()
+    miles_driven = float(input("Enter miles driven:\t\t"))
+    gallons_used = float(input("Enter gallons of gas used:\t\t"))
+    cost_per_gallon = float(input("Enter cost per gallon:\t\t")
+    print()
+    mpg = (miles_driven / gallons_used, 2)
+    tgc = round(mpg, 2)
+    cpm = round(tcp / miles_drivers, 1)         
+    print("Miles Per Gallon:\t\t" + str(mpg))
+    print("total gas cost:\t\t\t" + str(tgc))
+    print("cost per mile:\t\t\t" + str(cpm))
+    print()
+    choice = input("Get entries for another trip?" (y/n): ")
 print() 
 print("bye")
 
